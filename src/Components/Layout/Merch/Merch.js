8d19a7c5987898bcd4338f9d1331c1c59c806e21
krishnavarik <krishnavarik9@gmail.com>
2022-01-24 17:05:58 +0530

    added Merch.js

---
+++ src/Components/Layout/Merch/Merch.js
@@ -0,0 +1,14 @@
+import MerchProducts from "./MerchProducts";
+import './Merch.css';
+
+
+const Merch = () => {
+    return (
+        <div className="merchBody">
+            <h2>MERCH</h2>
+            <MerchProducts/>
+        </div>
+    )
+}
+
+export default Merch;
