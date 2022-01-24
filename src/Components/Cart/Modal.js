8c6dbe035e2aad116fde078bc27dd40b1114430b
krishnavarik <krishnavarik9@gmail.com>
2022-01-24 16:51:24 +0530

    added Modal.js

---
+++ src/Components/Cart/Modal.js
@@ -0,0 +1,34 @@
+import ReactDOM from "react-dom";
+import { Fragment } from "react";
+import "./Modal.css";
+
+const Backdrop = (props) => {
+  return <div className="backdrop" onClick={props.onClose} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className="modal">
+      <div className="content">{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("backDrop");
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
+
+export default Modal;
