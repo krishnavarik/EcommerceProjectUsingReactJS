8bf565417fb63b4daa23860fc55368304315faa8
krishnavarik <krishnavarik9@gmail.com>
2022-01-24 17:24:03 +0530

    added Store.js

---
+++ src/Components/Pages/Store.js
@@ -0,0 +1,13 @@
+import Merch from "../Layout/Merch/Merch";
+import Music from "../Layout/Music/Music";
+
+const Store = () => {
+  return (
+    <>
+      <Music />
+      <Merch />
+    </>
+  );
+};
+
+export default Store;
