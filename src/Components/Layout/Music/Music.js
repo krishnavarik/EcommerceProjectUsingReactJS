b2b80c29757e1f4fc15981e6351c9b32df0ee5b8
krishnavarik <krishnavarik9@gmail.com>
2022-01-24 17:13:45 +0530

    added Music.js

---
+++ src/Components/Layout/Music/Music.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Music.css'
+import MusicProducts from "./MusicProducts";
+const Music = (props) => {
+    return (
+    
+        <div className="musicBody">
+            <h2>MUSIC</h2>
+            <MusicProducts />
+        </div>
+        
+    )
+}
+
+export default Music;
