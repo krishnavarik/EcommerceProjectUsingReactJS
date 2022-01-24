3f59a6c2efa792151396a056a78744993e4da40f
krishnavarik <krishnavarik9@gmail.com>
2022-01-24 16:55:58 +0530

    added Footer.js

---
+++ src/Components/Layout/Footer/Footer.js
@@ -0,0 +1,10 @@
+import './Footer.css'
+const Footer = () =>{
+    return (
+        <div className="footerBody">
+            <h2>The Generics</h2>
+            
+        </div>
+    )
+}
+export default Footer;
